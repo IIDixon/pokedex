baaed6d90980d1238c5e77c68608fbfb54f2292b
Nathan Junio <79364641+IIDixon@users.noreply.github.com>
2022-04-27 16:12:38 -0300

    alter string error searching pokemon

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -272,7 +272,7 @@ class _HomePageState extends State<HomePage> {
                             child: Column(
                               children: const [
                                 Icon(Icons.error),
-                                Text("Nenhum pokémon encontrado com o nome digitado"),
+                                Text("No pokemon found with this name"),
                               ],
                             ),
                           );
