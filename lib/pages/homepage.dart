61056d05c4ea988fa0c6a0b119aad47fcb8629cf
IIDixon <nathan.991016626@gmail.com>
2022-04-28 17:02:58 -0300

    several

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -255,6 +255,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+              const SizedBox(height: 5,),
               Expanded(
                 child: FutureBuilder(
                   future: getPokemon(),
